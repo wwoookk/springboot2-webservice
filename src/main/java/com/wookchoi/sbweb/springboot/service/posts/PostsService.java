493d632aa7f9ab1971a65609829bf660475a01a4
wookchoi <wwoookk@gmail.com>
2020-02-08 15:16:15 +0900

    apply spring security

---
+++ src/main/java/com/wookchoi/sbweb/springboot/service/posts/PostsService.java
@@ -0,0 +1,58 @@
+package com.wookchoi.sbweb.springboot.service.posts;
+
+import com.wookchoi.sbweb.springboot.domain.posts.Posts;
+import com.wookchoi.sbweb.springboot.domain.posts.PostsRepository;
+import com.wookchoi.sbweb.springboot.web.dto.PostsListResponseDto;
+import com.wookchoi.sbweb.springboot.web.dto.PostsResponseDto;
+import com.wookchoi.sbweb.springboot.web.dto.PostsSaveRequestDto;
+import com.wookchoi.sbweb.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto postsSaveRequestDto) {
+        return postsRepository.save(postsSaveRequestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id)
+        );
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    @Transactional(readOnly = true)
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc()
+                .stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/com/wookchoi/sbweb/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,11 @@
+package com.wookchoi.sbweb.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+    private final String name;
+    private final int amount;
+}
